d80c91fc6efe6ffddb0adf598b8ed0204980e5fc
Chamele-listens <ratanakvisal.ny@gmail.com>
2022-11-26 17:20:09 +0700

    Added info.js that use sub-commands

--- commands/echo.js
+++ commands/echo.js
@@ -5,11 +5,23 @@ module.exports = {
     data: new SlashCommandBuilder()
 	.setName('echo')
 	.setDescription('Replies with your input!')
+
 	.addStringOption(option =>
 		option.setName('input')
-			.setDescription('The input to echo back')),
+			.setDescription('The input to echo back'))
+			
+
+	.addChannelOption(option =>
+		option.setName('channel')
+			.setDescription('The channel to echo into'))
+
+	.addBooleanOption(option =>
+		option.setName('ephemeral')
+			.setDescription('Whether or not the echo should be ephemeral')),
+
     async execute(interaction){
-        await interaction.reply('nothing yet');
+        const input = interaction.options.getString('input')
+		await interaction.reply(`The user replied "${input}"`);
 
     },
 
