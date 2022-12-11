296ead1332d24a7952a78d757e35aadb67ef48c4
Chamele-listens <ratanakvisal.ny@gmail.com>
2022-12-11 16:58:11 +0700

    Updated button.js

--- commands/button.js
+++ commands/button.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder ,ActionRowBuilder, ButtonBuilder, ButtonStyle, Events, EmbedBuilder } = require('discord.js');
+const { SlashCommandBuilder ,ActionRowBuilder, ButtonBuilder, ButtonStyle, Events, EmbedBuilder, ButtonComponent } = require('discord.js');
 //const { execute } = require('./translate');
 
 module.exports = {
@@ -13,13 +13,22 @@ module.exports = {
                 .setLabel('click me!')
                 .setStyle(ButtonStyle.Primary),
         ),
+    const :row2 = new ActionRowBuilder()
+    .addComponents(
+        new ButtonBuilder()
+            .setCustomId('pls_no')
+            .setLabel('Dont click on dis')
+            .setStyle(ButtonStyle.Danger),
+    ),
+
     const :embed = new EmbedBuilder()
             .setColor(0x0099FF)
             .setTitle('You asked for it!')
             .setURL('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
             .setDescription('perfectly normal button'),
 
-    async execute(interaction){
-        await interaction.reply({ content: 'I think you should,', embeds: [embed], components: [row] });
+    async execute(interaction){       
+        await interaction.reply({ content: 'I think you should,', embeds: [embed], components: [row, row2] });
+        
     }
 };
--- events/button_Interaction.js
+++ events/button_Interaction.js
@@ -1,11 +1,22 @@
 const { Events } = require('discord.js');
+//const button = require('../commands/button');
 
 module.exports ={
     name: Events.InteractionCreate,
     async execute(interaction){
         if (!interaction.isButton()) return;
 	    console.log(interaction);
-        await interaction.reply('it worked')
-        await interaction.followUp('Also, https://www.youtube.com/watch?v=dQw4w9WgXcQ')
+        const button_name = interaction.customId 
+
+        if (button_name === 'primary' ){
+            await interaction.reply('It worked!')
+        }else if(button_name === 'pls_no'){
+            await interaction.reply('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
+        }else{
+            console.log('something went wrong')
+        }
+
+        //await interaction.reply('it worked')
+        //await interaction.followUp('Also, https://www.youtube.com/watch?v=dQw4w9WgXcQ')
     }
 }
