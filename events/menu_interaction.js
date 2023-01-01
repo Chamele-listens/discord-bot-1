d1cd2add66a72fa34430d5ecb38e55a34006c824
Chamele-listens <ratanakvisal.ny@gmail.com>
2023-01-01 16:00:00 +0700

    Update menu_interaction.js

--- events/menu_interaction.js
+++ events/menu_interaction.js
@@ -6,6 +6,12 @@ module.exports = {
     async execute(interaction){
         if (!interaction.isSelectMenu()) return;
         console.log(interaction);
-        await interaction.reply('We got nothing yet! Please come back later! (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧')
+        const selected = interaction.values[0];
+        if (selected === 'first_option'){
+            await interaction.update({ content: 'This is u rn Q_Q', components: [] })
+        }else if (selected === 'second_option'){
+            await interaction.update({ content: 'That is you rn', components: [] })
+        }
+        //await interaction.reply('We got nothing yet! Please come back later! (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧')
     }
 }
